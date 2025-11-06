29eab1f1c51210197f93916149157b03c3d2e099
gwozdziu1017 <gwozdziu1017@o2.pl>
2025-11-06 21:02:27 +0100

    Extracting unnecessary vars and funcs from CipheredTextModel. Changing it into static struct

--- Enigma/Model/CipheredTextModel.swift
+++ Enigma/Model/CipheredTextModel.swift
@@ -44,48 +44,14 @@ let letterToIndexArray = [
 ]
 
 struct CipheredTextModel {
-    var openText: String
-    var cipheredText: String
-    var arrayOfCipheredText: [String]
-    
-    func getLetterIndex(_ letter: String) -> Int {
+    static func getLetterIndex(_ letter: String) -> Int {
         if letter.count == 1 && isValidLetter(letter) {
             return letterToIndexArray.firstIndex(of: letter.uppercased())! + 1
         }
         return 0
     }
     
-    func isValidLetter(_ letter: String) -> Bool {
+    static func isValidLetter(_ letter: String) -> Bool {
         return letterToIndexArray.contains(letter.uppercased()) // all valid letters must be in letterToIndexArray
-    }
-    
-    mutating func addLetterToCipheredText(_ letter: String) {
-        cipheredText.append(letter.uppercased())
-    }
-
-    func getCipheredText() -> String {
-        return String(cipheredText)
-    }
-
-    func splitStringIntoArrayOfFiveElementStrings(_ str: String) -> [String] {
-        var counter = 0
-        var tempString = ""
-        var newArray: [String] = []
-        
-        for letter in str {
-            tempString.append(letter)
-            counter += 1
-            if counter.isMultiple(of: 5) {
-                newArray.append(tempString)
-                tempString = ""
-            }
-        }
-        newArray.append(tempString)
-        return newArray
-    }
-
-    mutating func getArrayOfSplittedCipheredText() -> [String] {
-        arrayOfCipheredText = splitStringIntoArrayOfFiveElementStrings(cipheredText)
-        return arrayOfCipheredText
     }
 }
--- EnigmaTests/CipheredTextTestSuite.swift
+++ EnigmaTests/CipheredTextTestSuite.swift
@@ -9,7 +9,6 @@ import XCTest
 @testable import Enigma
 
 final class CipheredTextTestSuite: XCTestCase {
-    var sut = CipheredTextModel(openText: "", cipheredText: "", arrayOfCipheredText: [])
 
     override func setUpWithError() throws {
 
@@ -23,7 +22,7 @@ final class CipheredTextTestSuite: XCTestCase {
         let arrayOfPositiveCases = [ "A", "B", "C", "D", "e", "f", "z" ]
 
         for positiveCaseElement in arrayOfPositiveCases {
-            XCTAssert(sut.isValidLetter(positiveCaseElement))
+            XCTAssert(CipheredTextModel.isValidLetter(positiveCaseElement))
         }
     }
 
@@ -31,61 +30,62 @@ final class CipheredTextTestSuite: XCTestCase {
         let arrayOfNegativeCases = [ " ", "\n", "123", "xyz", "" ]
 
         for negativeCaseElement in arrayOfNegativeCases {
-            XCTAssertFalse(sut.isValidLetter(negativeCaseElement))
+            XCTAssertFalse(CipheredTextModel.isValidLetter(negativeCaseElement))
         }
     }
 
     func testGetLetterIndexReturnsRightIndexOrZero() {
-        XCTAssert(sut.getLetterIndex("a") == 1)
-        XCTAssert(sut.getLetterIndex("Z") == 26)
-        XCTAssert(sut.getLetterIndex(" ") == 0)
-    }
-
-    func testAddOneLetterLowercaseToCipheredTextGetCipheredTextReturnsUppercasedCipheredText() {
-        sut.addLetterToCipheredText("a")
-
-        XCTAssert(sut.cipheredText.count == 1 && sut.cipheredText == "A")
-        XCTAssertEqual(sut.getCipheredText(), "A")
-    }
-
-    func testAddOneLetterUppercaseToCipheredTextGetCipheredTextReturnsUnchangedLetterInCipheredText() {
-        sut.addLetterToCipheredText("X")
-
-        XCTAssert(sut.cipheredText.count == 1 && sut.cipheredText == "X")
-        XCTAssertEqual(sut.getCipheredText(), "X")
-    }
-
-    func testAddSixLettersToCipheredTextGetArrayOfCpheredTextReturnsTwoStrings() {
-        sut.addLetterToCipheredText("a")
-        sut.addLetterToCipheredText("b")
-        sut.addLetterToCipheredText("c")
-        sut.addLetterToCipheredText("d")
-        sut.addLetterToCipheredText("e")
-        sut.addLetterToCipheredText("f")
-
-        let expected = ["ABCDE", "F"]
-
-        XCTAssertEqual(sut.getArrayOfSplittedCipheredText(), expected)
-    }
-
-    func testAddSevenLettersToCipheredTextGetArrayOfCpheredTextReturnsTwoStrings() {
-        sut.addLetterToCipheredText("a")
-        sut.addLetterToCipheredText("b")
-        sut.addLetterToCipheredText("c")
-        sut.addLetterToCipheredText("d")
-        sut.addLetterToCipheredText("e")
-        sut.addLetterToCipheredText("f")
-        sut.addLetterToCipheredText("g")
-
-        let expected = ["ABCDE", "FG"]
-
-        XCTAssertEqual(sut.getArrayOfSplittedCipheredText(), expected)
-    }
-
-    func testAddTwoLettersToCipheredTextGetArrayOfCpheredTextReturnsOneString() {
-        sut.addLetterToCipheredText("z")
-        let expected = ["Z"]
-
-        XCTAssertEqual(sut.getArrayOfSplittedCipheredText(), expected)
+        XCTAssert(CipheredTextModel.getLetterIndex("a") == 1)
+        XCTAssert(CipheredTextModel.getLetterIndex("Z") == 26)
+        XCTAssert(CipheredTextModel.getLetterIndex(" ") == 0)
     }
 }
+
+//    func testAddOneLetterLowercaseToCipheredTextGetCipheredTextReturnsUppercasedCipheredText() {
+//        sut.addLetterToCipheredText("a")
+//
+//        XCTAssert(sut.cipheredText.count == 1 && sut.cipheredText == "A")
+//        XCTAssertEqual(sut.getCipheredText(), "A")
+//    }
+//
+//    func testAddOneLetterUppercaseToCipheredTextGetCipheredTextReturnsUnchangedLetterInCipheredText() {
+//        sut.addLetterToCipheredText("X")
+//
+//        XCTAssert(sut.cipheredText.count == 1 && sut.cipheredText == "X")
+//        XCTAssertEqual(sut.getCipheredText(), "X")
+//    }
+//
+//    func testAddSixLettersToCipheredTextGetArrayOfCpheredTextReturnsTwoStrings() {
+//        sut.addLetterToCipheredText("a")
+//        sut.addLetterToCipheredText("b")
+//        sut.addLetterToCipheredText("c")
+//        sut.addLetterToCipheredText("d")
+//        sut.addLetterToCipheredText("e")
+//        sut.addLetterToCipheredText("f")
+//
+//        let expected = ["ABCDE", "F"]
+//
+//        XCTAssertEqual(sut.getArrayOfSplittedCipheredText(), expected)
+//    }
+//
+//    func testAddSevenLettersToCipheredTextGetArrayOfCpheredTextReturnsTwoStrings() {
+//        sut.addLetterToCipheredText("a")
+//        sut.addLetterToCipheredText("b")
+//        sut.addLetterToCipheredText("c")
+//        sut.addLetterToCipheredText("d")
+//        sut.addLetterToCipheredText("e")
+//        sut.addLetterToCipheredText("f")
+//        sut.addLetterToCipheredText("g")
+//
+//        let expected = ["ABCDE", "FG"]
+//
+//        XCTAssertEqual(sut.getArrayOfSplittedCipheredText(), expected)
+//    }
+//
+//    func testAddTwoLettersToCipheredTextGetArrayOfCpheredTextReturnsOneString() {
+//        sut.addLetterToCipheredText("z")
+//        let expected = ["Z"]
+//
+//        XCTAssertEqual(sut.getArrayOfSplittedCipheredText(), expected)
+//    }
+
